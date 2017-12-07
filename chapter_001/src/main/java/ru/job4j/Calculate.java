d312949fb8708fa86c9e716dc741500bd933bc43
Darmandi <enot4you@gmail.com>
2017-12-07 03:05:58 +0300

    1.3. JavaDoc. Code convention [#4087]

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -1,6 +1,16 @@
 package ru.job4j;
 
+/**
+ * Calculate.
+ * @author Dmitrii Saraev (guitarpro4@mail.ru)
+ * @version 1
+ * @since 7.12.2017
+ */
 public class Calculate {
+	/**
+	 * Main.
+	 * @param args - args.
+	 */
 	public static void main(String[] args) {
 		System.out.println("Hello world");   
 	}
