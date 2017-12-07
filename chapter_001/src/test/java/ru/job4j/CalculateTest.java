fdeb99673fa1b3dce2c5d23295f7a1a42d8c9dff
Darmandi <enot4you@gmail.com>
2017-12-07 04:47:50 +0300

    1.4. JUnit [#4122]

---
+++ chapter_001/src/test/java/ru/job4j/CalculateTest.java
@@ -0,0 +1,27 @@
+package ru.job4j;
+
+import org.junit.Test;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+/**
+ * Test.
+ * @author Dmitrii Saraev (guitarpro4@mail.ru)
+ * @version 1
+ * @since 7.12.2017
+ */
+public class CalculateTest {
+	/**
+	 * Test echo.
+	 */ @Test
+	public void whenTakeNameThenTreeEchoPlusName() {
+	    String input = "Dmitrii Saraev";
+	    String expect = "Echo, echo, echo : Dmitrii Saraev"; 
+	    Calculate calc = new Calculate();
+	    String result = calc.echo(input);
+	    assertThat(result, is(expect));
+	}
+ 
+}
---
+++ chapter_001/src/test/java/ru/job4j/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * Package for calculate task.
+ *
+ * @author Dmitrii Saraev (guitarpro4@mail.ru)
+ * @version 1
+ * @since 7.12.2017
+ */
+package ru.job4j;
