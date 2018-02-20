47380de0f3b08ff14cb0f42c7edf85710517ba8e
Darmandi <enot4you@gmail.com>
2018-02-21 01:15:37 +0300

    3.2. Конвертер валюты. [#41583]

---
+++ chapter_001/src/main/java/ru/job4j/calculator/Fit.java
@@ -0,0 +1,29 @@
+package ru.job4j.calculator;
+
+/**
+ * Программа расчета идеального веса.
+ */
+public class Fit {
+	
+	private double weight;
+
+    /**
+     * Идеальный вес для мужщины.
+     * @param height Рост.
+     * @return идеальный вес.
+     */
+    double manWeight(double height) {
+		this.weight = (height-100)*1.15;
+        return -1D;
+    }
+
+    /**
+     * Идеальный вес для женщины.
+     * @param height Рост.
+     * @return идеальный вес.
+     */
+    double womanWeight(double height) {
+		this.weight = (height-110)*1.15;
+        return -1D;
+    }
+}
