9fbbf023133b7b04bdd5f307a3330b69ea15a461
Darmandi <enot4you@gmail.com>
2018-03-01 12:52:09 +0300

    3.4. Расстояние между точками в системе координат [#188]

--- chapter_001/src/main/java/ru/job4j/condition/Point.java
+++ chapter_001/src/main/java/ru/job4j/condition/Point.java
@@ -3,24 +3,24 @@ package ru.job4j.condition;
 public class Point {
 	private int x;
 	private int y;
-	private int z;
 
-	public Point(int dim1, int dim2, int dim3) {
+
+	public Point(int dim1, int dim2) {
 		this.x = dim1;
 		this.y = dim2;
-		this.z = dim3;
+
 	}
 
 	public double distance(Point second) {
-		return  Math.sqrt(Math.pow(this.x - second.x, 2) + Math.pow(this.y - second.y, 2) + Math.pow(this.z - second.z, 2));
+		return  Math.sqrt(Math.pow(this.x - second.x, 2) + Math.pow(this.y - second.y, 2));
 	}
 
 	public static void main(String[] args) {
-		Point a = new Point(1, 1, 1);
-		Point b = new Point(2, 2, 2);
+		Point a = new Point(1, 1);
+		Point b = new Point(2, 2);
 
-		System.out.println("x1 = " + a.x + "  y1 = " + a.y + "  z1 = " + a.z);
-		System.out.println("x2 = " + b.x + "  y2 = " + b.y + "  z2 = " + b.z);
+		System.out.println("x1 = " + a.x + "  y1 = " + a.y);
+		System.out.println("x2 = " + b.x + "  y2 = " + b.y);
 
 		System.out.println("Расстояние между точками А и В : " + a.distance(b));
 	}
