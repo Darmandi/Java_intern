1aab1adf83c039cbbb7f0e2f0fea32dd922f401a
Darmandi <enot4you@gmail.com>
2018-03-03 09:08:57 +0300

    4.1. Подсчет суммы чётных чисел в диапазоне

---
+++ chapter_001/src/main/java/ru/job4j/loop/Counter.java
@@ -0,0 +1,23 @@
+package ru.job4j.loop;
+
+/**
+ * @author Dmitrii Saraev
+ * @version 1
+ * @since 3.03.2018
+ */
+
+public class Counter {
+    /**
+     * Вычисляет сумму четных чисел в заданном диапазое
+     * @param begin начало диапазона
+     * @param end конец диапазона
+     */
+    public int sumOfEven(int begin, int end) {
+        int temp = 0;
+        for (int i = begin; i<= end; i++) {
+            if (i % 2 == 0)
+                temp = temp + i;
+        }
+        return temp;
+    }
+}
