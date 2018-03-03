c8796418fdf397991da9346ab8a67e4d20d9e302
Darmandi <enot4you@gmail.com>
2018-03-03 14:07:56 +0300

    3.2. Конвертер валюты

--- chapter_001/src/test/java/ru/job4j/calculator/ConverterTest.java
+++ chapter_001/src/test/java/ru/job4j/calculator/ConverterTest.java
@@ -5,19 +5,19 @@ import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
 public class ConverterTest {
-    private final int Rubineu = 70;
-    private final int Rubinusd = 60;
+    private final int RUBINEU = 70;
+    private final int RUBINUSD = 60;
     @Test
     public void when60RubleToDollarThen1() {
         Converter converter = new Converter();
-        int result = converter.rubleToDollar(Rubinusd);
+        int result = converter.rubleToDollar(RUBINUSD);
         assertThat(result, is(1));
     }
 
     @Test
     public void when70RubleToEuroThen1() {
         Converter converter = new Converter();
-        int result = converter.rubleToEuro(Rubineu);
+        int result = converter.rubleToEuro(RUBINEU);
         assertThat(result, is(1));
     }
 
@@ -25,13 +25,13 @@ public class ConverterTest {
     public void when1DollarTo60Ruble() {
         Converter converter = new Converter();
         int result = converter.dollarToRuble(1);
-        assertThat(result, is(Rubinusd));
+        assertThat(result, is(RUBINUSD));
     }
 
     @Test
     public void when1EuroTo70Ruble() {
         Converter converter = new Converter();
         int result = converter.euroToRuble(1);
-        assertThat(result, is(Rubineu));
+        assertThat(result, is(RUBINEU));
     }
 }
