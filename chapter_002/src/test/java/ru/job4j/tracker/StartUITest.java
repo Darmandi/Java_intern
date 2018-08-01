589605abc963a2534dd2d08c099ce13688e18ddd
Darmandi <enot4you@gmail.com>
2018-08-01 23:12:43 +0300

    Стажер 2.4.6. Доработать тесты в StartUITest проверяющие вывод данных в консоль

--- chapter_002/src/test/java/ru/job4j/tracker/StartUITest.java
+++ chapter_002/src/test/java/ru/job4j/tracker/StartUITest.java
@@ -77,4 +77,123 @@ public class StartUITest {
                 .append(System.lineSeparator())
                 .toString()));
     }
+    @Test
+    public void whenCheckConsoleOutputAddObject() {
+        Input input = new StubInput(new String[]{"0", "name4", "desc4", "6"});
+        new StartUI(input, tracker).init();
+        assertThat(new String(out.toByteArray()), is(new StringBuilder()
+                .append(System.lineSeparator())
+                .append("Меню.").append(System.lineSeparator())
+                .append("0. Add new Item").append(System.lineSeparator())
+                .append("1. Show all items").append(System.lineSeparator())
+                .append("2. Edit item").append(System.lineSeparator())
+                .append("3. Delete item").append(System.lineSeparator())
+                .append("4. Find item by Id").append(System.lineSeparator())
+                .append("5. Find items by name").append(System.lineSeparator())
+                .append("6. Exit Program").append(System.lineSeparator())
+                .append(System.lineSeparator())
+                .append("------------ Добавление новой заявки --------------").append(System.lineSeparator())
+                .append("Новая заявка с ID: ").append(tracker.getAll()[3].getID()).append(System.lineSeparator())
+                .append(System.lineSeparator())
+                .append("Меню.").append(System.lineSeparator())
+                .append("0. Add new Item").append(System.lineSeparator())
+                .append("1. Show all items").append(System.lineSeparator())
+                .append("2. Edit item").append(System.lineSeparator())
+                .append("3. Delete item").append(System.lineSeparator())
+                .append("4. Find item by Id").append(System.lineSeparator())
+                .append("5. Find items by name").append(System.lineSeparator())
+                .append("6. Exit Program").append(System.lineSeparator())
+                .append(System.lineSeparator())
+                .toString()));
+    }
+    @Test
+    public void whenCheckConsoleOutputUpdateObject() {
+        Input input = new StubInput(new String[]{"2", item1.getID(), "edited name", "edited desc", "6"});
+        new StartUI(input, tracker).init();
+        assertThat(new String(out.toByteArray()), is(new StringBuilder()
+                .append(System.lineSeparator())
+                .append("Меню.").append(System.lineSeparator())
+                .append("0. Add new Item").append(System.lineSeparator())
+                .append("1. Show all items").append(System.lineSeparator())
+                .append("2. Edit item").append(System.lineSeparator())
+                .append("3. Delete item").append(System.lineSeparator())
+                .append("4. Find item by Id").append(System.lineSeparator())
+                .append("5. Find items by name").append(System.lineSeparator())
+                .append("6. Exit Program").append(System.lineSeparator())
+                .append(System.lineSeparator())
+                .append("------------ Изменение заявки --------------").append(System.lineSeparator())
+                .append("Заявка отредактирована. ID заявки: ").append(item1.getID()).append(System.lineSeparator())
+                .append(System.lineSeparator())
+                .append("Меню.").append(System.lineSeparator())
+                .append("0. Add new Item").append(System.lineSeparator())
+                .append("1. Show all items").append(System.lineSeparator())
+                .append("2. Edit item").append(System.lineSeparator())
+                .append("3. Delete item").append(System.lineSeparator())
+                .append("4. Find item by Id").append(System.lineSeparator())
+                .append("5. Find items by name").append(System.lineSeparator())
+                .append("6. Exit Program").append(System.lineSeparator())
+                .append(System.lineSeparator())
+                .toString()));
+    }
+    @Test
+    public void whenCheckConsoleOutputDeleteObject() {
+        String id2 = item2.getID();
+        Input input = new StubInput(new String[]{"3", id2, "6"});
+        new StartUI(input, tracker).init();
+        assertThat(new String(out.toByteArray()), is(new StringBuilder()
+                .append(System.lineSeparator())
+                .append("Меню.").append(System.lineSeparator())
+                .append("0. Add new Item").append(System.lineSeparator())
+                .append("1. Show all items").append(System.lineSeparator())
+                .append("2. Edit item").append(System.lineSeparator())
+                .append("3. Delete item").append(System.lineSeparator())
+                .append("4. Find item by Id").append(System.lineSeparator())
+                .append("5. Find items by name").append(System.lineSeparator())
+                .append("6. Exit Program").append(System.lineSeparator())
+                .append(System.lineSeparator())
+                .append("------------ Удаление заявки --------------").append(System.lineSeparator())
+                .append("Заявка с ID ").append(id2).append(" удалена").append(System.lineSeparator())
+                .append(System.lineSeparator())
+                .append("Меню.").append(System.lineSeparator())
+                .append("0. Add new Item").append(System.lineSeparator())
+                .append("1. Show all items").append(System.lineSeparator())
+                .append("2. Edit item").append(System.lineSeparator())
+                .append("3. Delete item").append(System.lineSeparator())
+                .append("4. Find item by Id").append(System.lineSeparator())
+                .append("5. Find items by name").append(System.lineSeparator())
+                .append("6. Exit Program").append(System.lineSeparator())
+                .append(System.lineSeparator())
+                .toString()));
+    }
+    @Test
+    public void whenCheckConsoleOutputShowAllObjects() {
+        Input input = new StubInput(new String[]{"1", "6"});
+        new StartUI(input, tracker).init();
+        assertThat(new String(out.toByteArray()), is(new StringBuilder()
+                .append(System.lineSeparator())
+                .append("Меню.").append(System.lineSeparator())
+                .append("0. Add new Item").append(System.lineSeparator())
+                .append("1. Show all items").append(System.lineSeparator())
+                .append("2. Edit item").append(System.lineSeparator())
+                .append("3. Delete item").append(System.lineSeparator())
+                .append("4. Find item by Id").append(System.lineSeparator())
+                .append("5. Find items by name").append(System.lineSeparator())
+                .append("6. Exit Program").append(System.lineSeparator())
+                .append(System.lineSeparator())
+                .append("------------ Все заявки --------------").append(System.lineSeparator())
+                .append("ID: ").append(item1.getID()).append(" Имя заявки: ").append(item1.getName()).append(System.lineSeparator())
+                .append("ID: ").append(item2.getID()).append(" Имя заявки: ").append(item2.getName()).append(System.lineSeparator())
+                .append("ID: ").append(item3.getID()).append(" Имя заявки: ").append(item3.getName()).append(System.lineSeparator())
+                .append(System.lineSeparator())
+                .append("Меню.").append(System.lineSeparator())
+                .append("0. Add new Item").append(System.lineSeparator())
+                .append("1. Show all items").append(System.lineSeparator())
+                .append("2. Edit item").append(System.lineSeparator())
+                .append("3. Delete item").append(System.lineSeparator())
+                .append("4. Find item by Id").append(System.lineSeparator())
+                .append("5. Find items by name").append(System.lineSeparator())
+                .append("6. Exit Program").append(System.lineSeparator())
+                .append(System.lineSeparator())
+                .toString()));
+    }
 }
