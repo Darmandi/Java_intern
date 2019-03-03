ba0854f173bffac30c8bacf0f8deee0d15943bed
Darmandi <enot4you@gmail.com>
2019-03-03 08:02:28 +0300

    Стажер 3.1.3. Конвертация ArrayList в двухмерный массив Удаление излишнего заполнения нулями

--- chapter_003/src/main/java/ru/job4j/list/ConvertList2Array.java
+++ chapter_003/src/main/java/ru/job4j/list/ConvertList2Array.java
@@ -12,16 +12,10 @@ public class ConvertList2Array {
     public int[][] toArray(List<Integer> list, int rows) {
         int cells = (int) Math.ceil((double) list.size() / rows);
         int[][] array = new int[rows][cells];
-        int index = 0;
         int indexrow = 0;
         int indexcell = 0;
         for (int elem : list) {
-            if (index + 1 > list.size()) {
-                array[indexrow][indexcell] = 0;
-            } else {
                 array[indexrow][indexcell] = elem;
-                index++;
-            }
             if (indexcell < cells - 1) {
                 indexcell++;
             } else {
