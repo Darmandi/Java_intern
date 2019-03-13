247d942a00baed8b07ad641d4bdfb3dc407d40e5
Major-Miau <enot4you@gmail.com>
2019-03-13 12:35:27 +0300

    Стажер 3.3.3. Компаратор для строк

---
+++ chapter_003/src/main/java/ru/job4j/sort/ListCompare.java
@@ -0,0 +1,29 @@
+package ru.job4j.sort;
+
+import java.util.Comparator;
+/**
+ * @author Dmitrii Saraev
+ * @version 0.1
+ */
+public class ListCompare implements Comparator<String> {
+    /**
+     * Метод сравнивает две строки посимвольно
+     * @param left первая строка
+     * @param right вторая строка
+     * @return результат сравнения
+     */
+    @Override
+    public int compare(String left, String right) {
+        int length = left.length() > right.length() ? right.length() : left.length();
+        int result = 0;
+        for (int i = 0; i < length; i++) {
+            int res = Integer.compare(left.charAt(i), right.charAt(i));
+            if (res != 0) {
+                result = res;
+                break;
+            }
+        }
+        result = result != 0 ? result : Integer.compare(left.length(), right.length());
+        return result;
+    }
+}
