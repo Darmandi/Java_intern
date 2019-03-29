eda2089a897199f038a3695adb2e6a61a41e5f84
Major-Miau <enot4you@gmail.com>
2019-03-29 19:36:00 +0300

    Стажер 4.2.5. Преобразование матрицы чисел в список чисел

---
+++ chapter_004/src/main/java/ru/job4j/flatmap/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * Пакет для изучения Stream.of, flatMap
+ * @author Дмитрий Сараев (guitarpro4@mail.ru)
+ */
+package ru.job4j.flatmap;
---
+++ chapter_004/src/test/java/ru/job4j/flatmap/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * Пакет для тестов Stream.of, flatMap
+ * @author Дмитрий Сараев (guitarpro4@mail.ru)
+ */
+package ru.job4j.flatmap;
