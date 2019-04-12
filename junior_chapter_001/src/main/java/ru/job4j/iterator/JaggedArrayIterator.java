15d3f3abd1ca6ed418ec269d5a15ac9450a7138d
Major-Miau <enot4you@gmail.com>
2019-04-12 23:19:43 +0300

    Junior 1.1.1. Итератор для двухмерного массива int[][]

---
+++ junior_chapter_001/src/main/java/ru/job4j/iterator/JaggedArrayIterator.java
@@ -0,0 +1,55 @@
+package ru.job4j.iterator;
+
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+/**
+ * Класс для итерации по двумерному массиву
+ * @author Дмитрий Сараев (guitarpro4@mail.ru)
+ * @version 1
+ */
+public class JaggedArrayIterator implements Iterator {
+    private final int[][] value;
+    private int index3 = 0;
+    private int index4 = 0;
+
+    /**
+     * Класс с массивом
+     * @param value массив
+     */
+    public JaggedArrayIterator(final int[][] value) {
+        this.value = value;
+    }
+
+    /**
+     * Метод проверяет, выходит ли каретка за пределы массива
+     * @return false если выходит
+     */
+    @Override
+    public boolean hasNext() {
+        boolean res = true;
+        int index1 = index3;
+        if (value.length == index1) {
+            res = false;
+        }
+        return res;
+    }
+
+    /**
+     * Метод возвращает ячейку двумерного массива и сдвигает каретку на следующую ячейку
+     * @return ячейка массива
+     */
+    @Override
+    public Object next() {
+        int res;
+        if (value[index3].length == index4 + 1) {
+            res = value[index3][index4];
+            index4 = 0;
+            index3++;
+        } else {
+            res = value[index3][index4];
+            index4++;
+        }
+        return res;
+    }
+}
---
+++ junior_chapter_001/src/main/java/ru/job4j/iterator/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * Пакет для изучения итератора
+ * @author Дмитрий Сараев (guitarpro4@mail.ru)
+ */
+package ru.job4j.iterator;
